999ad106ed1628961ad67c1634b51b7d612a5c1b
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-08-22 19:49:25 +0200

    Adding global constant

--- 01-boing/app.py
+++ 01-boing/app.py
@@ -22,3 +22,13 @@ else:
 
 print("Let's ready to start the game")
 
+
+WIDTH = 800
+HEIGHT = 480
+TITLE = "Boing!"
+
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+PLAYER_SPEED = 6
+MAX_AI_SPEED = 6
+
